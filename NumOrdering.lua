3d6168a230cf2b3f9fe835cff6d81c88cd6ac917
ruilov <ruilov@gmail.com>
2012-06-30 12:09:36 -0700

    test

--- NumOrdering.lua
+++ NumOrdering.lua
@@ -1,1 +1,8 @@
-NumOrdering = class(
+NumOrdering = class(Panel)
+
+function NumOrdering:init()
+    Panel.init(self,0,0)
+    self.nx,self.ny = 5,5
+    self:makeButtons()
+    self.time = 0
+    self.title = TextElem("Time",WIDTH/2,HEIGHT-100,
