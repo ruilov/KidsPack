ffe18b230c3c831131906a5774d3c572e07dbbd2
ruilov <ruilov@gmail.com>
2012-06-30 12:20:29 -0700

    test

--- TextBanner.lua
+++ TextBanner.lua
@@ -35,4 +35,50 @@ end
 
 function TextBanner:moveCB()
     self.verts = self:createVerts()
-    self.myMesh.vertices = triangulat
+    self.myMesh.vertices = triangulate(self.verts)
+    self:recolor()
+end
+
+-- for a translation we don't have to call moveCB so let's overwrite that
+-- method. This saves a lot of speed
+function TextBanner:translate(dx,dy)
+    self.x = self.x + dx
+    self.y = self.y + dy
+end
+
+function TextBanner:draw()
+    pushMatrix()
+    pushStyle()
+    translate(self.x,self.y)
+    
+    -- draw the background
+    self.myMesh:draw()
+
+    -- draw the text
+    self.textElem:draw()
+    
+    -- draw the border
+    self:drawLines(self.verts)
+    popStyle()
+    popMatrix()
+end
+
+function TextBanner:createVerts()
+    local w,h = self.w,self.h 
+    local v = {}
+    if w == 0 then return v end
+    
+    local r = 1    
+    if self.type == "round" then    
+        v[1] = vec2(w,6*r)
+        v[2] = vec2(w-r,4*r)
+        v[3] = vec2(w-2*r,2*r)
+        v[4] = vec2(w-4*r,r)
+        v[5] = vec2(w-6*r,0)
+        
+        v[6] = vec2(6*r,0)
+        v[7] = vec2(4*r,r)
+        v[8] = vec2(2*r,2*r)
+        v[9] = vec2(r,4*r)
+        v[10] = vec2(0,6*r)
+     
